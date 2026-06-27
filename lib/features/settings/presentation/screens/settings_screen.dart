2c0c91f2399b86e1032a7d4e60cf11714de361fb
MeiGaming <meigaming.official@gmail.com>
2026-06-27 16:00:50 +0530

    chore: remove unused local legal markdown files from assets and project

--- lib/features/settings/presentation/screens/settings_screen.dart
+++ lib/features/settings/presentation/screens/settings_screen.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_foreground_task/flutter_foreground_task.dart';
